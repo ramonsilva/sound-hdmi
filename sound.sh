3906075c671459762eef171d330600047feb11af
Ramon Silva <raraujo.jpa@gmail.com>
2012-06-30 09:18:23 -0300

    removing comments unnecessaries

--- sound.sh
+++ sound.sh
@@ -1,17 +1,16 @@
 #!/bin/bash
 
-#USER=$1
 USER=$(/usr/bin/who | /usr/bin/cut -f1 -d" ")
 
 TEST=$(cat /sys/class/drm/card0-HDMI-A-1/status)
 
 if [ $TEST = connected ]; then
-  su $USER -c "env DISPLAY=:0.0 notify-send -u normal -t 500 -i gnome-sound-properties \"HDMI On $USER2\" \"Sound to HDMI $1\""
+  su $USER -c "env DISPLAY=:0.0 notify-send -u normal -t 500 -i gnome-sound-properties \"HDMI On\" \"Sound to HDMI\""
 
   su $USER -c "pacmd \"set-card-profile 0 output:hdmi-stereo\" > /dev/null 2>&1"
 
 else
-  su $USER -c "env DISPLAY=:0.0 notify-send -u normal -t 500 -i gnome-sound-properties \"HDMI Off $USER\" \"Sound to Internal\""
+  su $USER -c "env DISPLAY=:0.0 notify-send -u normal -t 500 -i gnome-sound-properties \"HDMI Off\" \"Sound to Internal\""
 
   su $USER -c "pacmd \"set-card-profile 0 output:analog-stereo\" > /dev/null 2>&1"
 
